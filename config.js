1f1f3bba0d629b6c7e652921eec3ed4d6ddded5b
dmibots <101457673+dmibots@users.noreply.github.com>
2022-05-09 18:31:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,18 +16,16 @@ second: 'numeric'
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}global.owner = ['6282146092695'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+}global.owner = ['6285172206884'] // Put your number here
+global.mods = ['6281268414805'] // Want some help?
+global.prems = ['62811666884'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com', //hmm free gk sih gk pernah nyoba soalnya :v
     bx: 'https://bx-hunter.herokuapp.com',
-  dhnjing: 'https://dhnjing.xyz', // mati kyk nya ygy 
   hardianto: 'https://hardianto.xyz', //free ygy
   jonaz: 'https://jonaz-api-v2.herokuapp.com', // shine kyk nya ygy
   neoxr: 'https://neoxr-api.herokuapp.com', // sama ini juga kyk nya
-  nrtm: 'https://nurutomo.herokuapp.com', // mati kyk nya ygy
   xteam: 'https://api.xteam.xyz', //gk recommend soalnya pelit gk free gk a6 btw bayar buat premium sama unlimited limit ygy
   nzcha: 'http://nzcha-apii.herokuapp.com',
   bg: 'http://bochil.ddns.net', // udah mati kyl nya eror mlu soalnya
@@ -64,19 +62,19 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Rika'
+global.packname = 'Ayra'
 global.author = '>////<'
 
 
 
 
 
-ig: 'ahmdlui'
-global.wm = 'SoikemMd'
+ig: 'tammyartha'
+global.wm = 'apamd'
 global.watermark = 'wm'
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
-global.media = 'https://telegra.ph/file/e001198ced5650ef53d47.jpg'
-global.instagram = 'https://instagram.com/ahmdlui'
+global.media = 'https://telegra.ph/file/f09f40f4a37713f73de30.jpg'
+global.instagram = 'https://instagram.com/tammyartha'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
